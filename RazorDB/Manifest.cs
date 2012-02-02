9daf2759ba77c230dd7736edf883c6df65e9f134
Peter Waldschmidt <peterw@gnoso.com>
2012-02-02 09:50:00 -0500

    Allow Manifest to grow as a log for 1000 items before rolling over to speed up the commit time.

--- RazorDB/Manifest.cs
+++ RazorDB/Manifest.cs
@@ -19,6 +19,11 @@ namespace RazorDB {
             get { return _baseFileName; }
         }
 
+        private int _manifestVersion = 0;
+        public int ManifestVersion {
+            get { return _manifestVersion; }
+        }
+
         private const int num_versions = 10;
         private int[] _versions = new int[num_versions];
         public int CurrentVersion(int level) {
@@ -45,42 +50,72 @@ namespace RazorDB {
             string manifestFile = Config.ManifestFile(_baseFileName);
             string tempManifestFile = manifestFile + "~";
 
-            // Make a backup of the current manifest file
-            if (File.Exists(manifestFile))
-                File.Move(manifestFile, tempManifestFile);
+            _manifestVersion++;
 
-            FileStream fs = new FileStream(manifestFile, FileMode.CreateNew, FileAccess.Write, FileShare.None, 1024, false);
-            BinaryWriter writer = new BinaryWriter(fs);
+            if (ManifestVersion > Config.ManifestVersionCount) {
+                // Make a backup of the current manifest file
+                if (File.Exists(manifestFile))
+                    File.Move(manifestFile, tempManifestFile);
 
-            try {
-                writer.Write(_versions.Length);
-                foreach (var b in _versions) {
-                    writer.Write(b);
+                FileStream fs = new FileStream(manifestFile, FileMode.CreateNew, FileAccess.Write, FileShare.None, 1024, false);
+                BinaryWriter writer = new BinaryWriter(fs);
+
+                try {
+                    WriteManifestContents(writer);
+                    writer.Close();
+                } catch {
+                    writer.Close();
+                    File.Delete(manifestFile);
+                    File.Move(tempManifestFile, manifestFile);
+                    throw;
                 }
-                writer.Close();
-            } catch {
-                writer.Close();
-                File.Delete(manifestFile);
-                File.Move(tempManifestFile, manifestFile);
-                throw;
+
+                // Delete the backup file
+                if (File.Exists(tempManifestFile))
+                    File.Delete(tempManifestFile);
+            } else {
+                FileStream fs = new FileStream(manifestFile, FileMode.Append, FileAccess.Write, FileShare.None, 1024, false);
+                BinaryWriter writer = new BinaryWriter(fs);
+                try {
+                    WriteManifestContents(writer);
+                } finally {
+                    writer.Close();
+                }
+            }
+        }
+
+        private void WriteManifestContents(BinaryWriter writer) {
+            long startPos = writer.BaseStream.Position;
+
+            writer.Write(ManifestVersion);
+            writer.Write(_versions.Length);
+            foreach (var b in _versions) {
+                writer.Write(b);
             }
 
-            // Delete the backup file
-            if (File.Exists(tempManifestFile))
-                File.Delete(tempManifestFile);
+            int size = (int)(writer.BaseStream.Position - startPos);
+            writer.Write(size);
         }
 
         private void Read() {
 
             string manifestFile = Config.ManifestFile(_baseFileName);
             if (!File.Exists(manifestFile)) {
-                Write(); // Save current defaults
                 return;
             }
 
             FileStream fs = new FileStream(manifestFile, FileMode.Open, FileAccess.Read, FileShare.None, 1024, false);
             BinaryReader reader = new BinaryReader(fs);
+
+            // Get the size of the last manifest block
+            reader.BaseStream.Seek(-4, SeekOrigin.End);
+            int size = reader.ReadInt32();
+
+            // Now seek to that position and read it
+            reader.BaseStream.Seek(-size - 4, SeekOrigin.End);
+
             try {
+                _manifestVersion = reader.ReadInt32();
                 int num_versions = reader.ReadInt32();
                 for (int i = 0; i < num_versions; i++) {
                     _versions[i] = reader.ReadInt32();
--- RazorDBTests/ManifestTests.cs
+++ RazorDBTests/ManifestTests.cs
@@ -6,6 +6,7 @@ using NUnit.Framework;
 using RazorDB;
 using System.IO;
 using System.Diagnostics;
+using System.Threading;
 
 namespace RazorDBTests {
 
@@ -27,11 +28,14 @@ namespace RazorDBTests {
             Assert.AreEqual(0, mf.CurrentVersion(0));
             Assert.AreEqual(1, mf.NextVersion(0));
             Assert.AreEqual(1, mf.CurrentVersion(0));
+            Assert.AreEqual(1, mf.ManifestVersion);
 
             var mf2 = new Manifest(path);
+            Assert.AreEqual(1, mf2.ManifestVersion);
             Assert.AreEqual(1, mf2.CurrentVersion(0));
             Assert.AreEqual(2, mf2.NextVersion(0));
             Assert.AreEqual(2, mf2.CurrentVersion(0));
+            Assert.AreEqual(2, mf2.ManifestVersion);
         }
 
         [Test]
@@ -49,15 +53,50 @@ namespace RazorDBTests {
 
             var timer = new Stopwatch();
             timer.Start();
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < 1100; i++) {
                 Assert.AreEqual(i+1, mf.NextVersion(0));
             }
             timer.Stop();
 
-            Console.WriteLine("Committed manifest update in average of {0} ms", (double)timer.ElapsedMilliseconds / 100.0);
+            Console.WriteLine("Committed manifest update in average of {0} ms", (double)timer.ElapsedMilliseconds / 1100.0);
 
             var mf2 = new Manifest(path);
-            Assert.AreEqual(100, mf2.CurrentVersion(0));
+            Assert.AreEqual(1100, mf2.CurrentVersion(0));
+        }
+
+        [Test]
+        public void WriteAndReadManifestThreaded() {
+
+            var path = Path.GetFullPath("WriteAndReadManifestMany");
+
+            // Remove the file if it exists
+            var filename = Path.ChangeExtension(path, "mf");
+            if (File.Exists(filename))
+                File.Delete(filename);
+
+            var mf = new Manifest(path);
+            Assert.AreEqual(0, mf.CurrentVersion(0));
+
+            int num_threads = 11;
+            List<Thread> threads = new List<Thread>();
+            for (int t = 0; t < num_threads; t++) {
+                threads.Add(new Thread(() => {
+                    for (int i = 0; i < 100; i++) {
+                        mf.NextVersion(0);
+                    }
+                }));
+            }
+
+            var timer = new Stopwatch();
+            timer.Start();
+            threads.ForEach((t) => t.Start());
+            threads.ForEach((t) => t.Join());
+            timer.Stop();
+
+            Console.WriteLine("Committed manifest update in average of {0} ms", (double)timer.ElapsedMilliseconds / 1100.0);
+
+            var mf2 = new Manifest(path);
+            Assert.AreEqual(1100, mf2.CurrentVersion(0));
         }
     }
 }
