6b08268eebb25d6a1b4e3e9f04759a388293f960
Peter Waldschmidt <peterw@gnoso.com>
2012-02-13 17:38:07 -0500

    Clean up the semaphore in the Close so we don't block multiple closes.

--- RazorDB/KeyValueStore.cs
+++ RazorDB/KeyValueStore.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.IO;
+using System.Diagnostics;
 
 namespace RazorDB {
     
@@ -161,14 +162,14 @@ namespace RazorDB {
 
         private object memTableRotationLock = new object();
         private JournaledMemTable _rotatedJournaledMemTable;
-        private AutoResetEvent _rotationGate = new AutoResetEvent(true);
+        private Semaphore _rotationSemaphore = new Semaphore(1, 1);
 
 #pragma warning disable 420
         public void RotateMemTable() {
             lock (memTableRotationLock) {
                 // Double check the flag in case we have multiple threads that make it into this routine
                 if (_currentJournaledMemTable.Full) {
-                    _rotationGate.WaitOne();    // Wait for the rotation gate to be open, and automatically reset once a single thread gets through.
+                    _rotationSemaphore.WaitOne();    // Wait for the rotation gate to be open, and automatically reset once a single thread gets through.
 
                     _rotatedJournaledMemTable = Interlocked.Exchange<JournaledMemTable>(ref _currentJournaledMemTable, new JournaledMemTable(_manifest.BaseFileName, _manifest.NextVersion(0)));
 
@@ -177,7 +178,7 @@ namespace RazorDB {
                             _rotatedJournaledMemTable.WriteToSortedBlockTable(_manifest);
                             _rotatedJournaledMemTable = null;
                         } finally {
-                            _rotationGate.Set(); // Open the gate for the next rotation
+                            _rotationSemaphore.Release(); // Open the gate for the next rotation
                         }
                     });
                 }
@@ -191,7 +192,10 @@ namespace RazorDB {
 
         public void Close() {
             // Make sure any inflight rotations have occurred before shutting down.
-            _rotationGate.WaitOne();
+            if (!_rotationSemaphore.WaitOne(30000))
+                throw new TimeoutException("Timed out waiting for table rotation to complete.");
+            // Release again in case another thread tries to close it again.
+            _rotationSemaphore.Release();
 
             if (_tableManager != null) {
                 _tableManager.Close();
@@ -201,6 +205,8 @@ namespace RazorDB {
                 _currentJournaledMemTable.Close();
                 _currentJournaledMemTable = null;
             }
+            // Don't finalize since we already closed it.
+            GC.SuppressFinalize(this);
         }
     }
 
