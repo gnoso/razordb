30f0ac095d584e8607edf2e493ed34f76121f6a6
Peter Waldschmidt <peterw@gnoso.com>
2012-02-20 11:11:40 -0500

    Add LRU cache implementation, and replace the IndexCache.

--- RazorDB/Cache.cs
+++ RazorDB/Cache.cs
@@ -6,56 +6,92 @@ using System.Text;
 namespace RazorDB {
 
     internal class CacheEntry<T> {
+        internal ByteArray Key;
         internal T Value;
+        internal int Size;
         internal LinkedListNode<CacheEntry<T>> ListNode;
     }
 
     public class Cache<T> {
-        public Cache(int sizeLimit) {
+        public Cache(int sizeLimit, Func<T,int> sizer) {
             _sizeLimit = sizeLimit;
+            _sizer = sizer;
         }
         private int _sizeLimit;
         private int _currentSize;
-        private Dictionary<ByteArray, CacheEntry<T>> _hash = new Dictionary<ByteArray,CacheEntry<T>>();
+        public int CurrentSize {
+            get { return _currentSize; }
+        }
+        private Func<T, int> _sizer;
+
+        private Dictionary<ByteArray, CacheEntry<T>> _hash = new Dictionary<ByteArray, CacheEntry<T>>();
         private LinkedList<CacheEntry<T>> _list = new LinkedList<CacheEntry<T>>();
-    
+        private object _lock = new object();
+
         public bool TryGetValue(ByteArray key, out T value) {
-            CacheEntry<T> val;
-            bool res = _hash.TryGetValue(key, out val);
-            value = res ? val.Value : default(T);
-            return res;
+            lock (_lock) {
+                CacheEntry<T> val;
+                bool exists = _hash.TryGetValue(key, out val);
+                if (exists) {
+                    // move the item to the top of the LRU list
+                    _list.Remove(val.ListNode);
+                    _list.AddFirst(val.ListNode);
+                }
+                // Set the output parameter
+                value = exists ? val.Value : default(T);
+                return exists;
+            }
         }
 
         public void Set(ByteArray key, T value) {
-            var ce = new CacheEntry<T> { Value = value };
-            var node = _list.AddFirst(ce);
-            ce.ListNode = node;
-            _hash.Add(key,ce);
+            lock (_lock) {
+                var cacheEntry = new CacheEntry<T> { Value = value, Size = _sizer(value), Key = key };
+                var node = _list.AddFirst(cacheEntry);
+                cacheEntry.ListNode = node;
+                _hash.Add(key, cacheEntry);
+                _currentSize += _sizer(value);
+
+                CheckCacheSizeAndEvict();
+            }
+        }
+
+        private void CheckCacheSizeAndEvict() {
+            while (_currentSize > _sizeLimit) {
+
+                var lastEntry = _list.Last;
+                var cacheEntry = lastEntry.Value;
+
+                // Subtract the last entry from the size
+                _currentSize -= cacheEntry.Size;
+                // Remove from list
+                _list.RemoveLast();
+                // Remove from hash
+                _hash.Remove(cacheEntry.Key);
+            }
         }
     }
     
     public class RazorCache {
 
         public RazorCache() {
-            _indexCache = new Dictionary<ByteArray, ByteArray[]>();
-            _blockTableIndexCache = new Dictionary<string, WeakReference>();
+            _blockIndexCache = new Cache<ByteArray[]>(Config.IndexCacheSize, index => index.Sum(ba => ba.Length));
         }
 
-        private Dictionary<string, WeakReference> _blockTableIndexCache;
-        private Dictionary<ByteArray, ByteArray[]> _indexCache;
+        private Cache<ByteArray[]> _blockIndexCache;
 
         public ByteArray[] GetBlockTableIndex(string baseName, int level, int version) {
+
             string fileName = Config.SortedBlockTableFile(baseName, level, version);
-            WeakReference indexRef;
-            if (_blockTableIndexCache.TryGetValue(fileName, out indexRef)) {
-                object idx = indexRef.Target;
-                if (idx != null)
-                    return (ByteArray[])idx;
+            ByteArray key = new ByteArray(Encoding.UTF8.GetBytes(fileName));
+            ByteArray[] index;
+
+            if (_blockIndexCache.TryGetValue(key, out index)) {
+                return index;
             }
             var sbt = new SortedBlockTable(baseName, level, version);
             try {
-                ByteArray[] index = sbt.GetIndex();
-                _blockTableIndexCache[fileName] = new WeakReference(index);
+                index = sbt.GetIndex();
+                _blockIndexCache.Set(key, index);
                 return index;
             } finally {
                 sbt.Close();
---
+++ RazorDBTests/CacheTests.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using RazorDB;
+
+namespace RazorDBTests {
+
+    [TestFixture]
+    public class CacheTests {
+
+        [Test]
+        public void BasicAdd() {
+
+            var cache = new Cache<ByteArray>(500 * 1024, ba => ba.Length );
+            var items = new List<KeyValuePair<ByteArray, ByteArray>>();
+
+            for (int i = 0; i < 100; i++) {
+                var key = ByteArray.Random(40);
+                var val = ByteArray.Random(256);
+                cache.Set(key, val);
+                items.Add(new KeyValuePair<ByteArray, ByteArray>(key, val));
+            }
+
+            ByteArray oval;
+            for (int i = 0; i < 100; i++) {
+                var data = items[i];
+                Assert.IsTrue(cache.TryGetValue(data.Key, out oval));
+                Assert.AreEqual(data.Value, oval);
+            }
+            Assert.IsFalse(cache.TryGetValue(ByteArray.Random(40), out oval));
+        }
+
+        [Test]
+        public void CheckAddWithSize() {
+
+            var cache = new Cache<ByteArray>(500 * 1024, ba => ba.Length);
+
+            int size = 0;
+            for (int i = 0; i < 100; i++) {
+                var key = ByteArray.Random(40);
+                var val = ByteArray.Random(256);
+                cache.Set(key, val);
+                size += val.Length;
+                Assert.AreEqual(size, cache.CurrentSize);
+            }
+        }
+
+        [Test]
+        public void CheckOverSizeLimit() {
+
+            int limit = 100 * 256;
+            var cache = new Cache<ByteArray>(limit, ba => ba.Length);
+            var items = new List<ByteArray>();
+
+            int size = 0;
+            for (int i = 0; i < 200; i++) {
+                var key = ByteArray.Random(40);
+                var val = ByteArray.Random(256);
+                cache.Set(key, val);
+                size += val.Length;
+
+                items.Add(key);
+            }
+
+            Assert.GreaterOrEqual(limit, cache.CurrentSize);
+            ByteArray output;
+            // First 100 items should have been evicted
+            for (int i = 0; i < 100; i++) {
+                Assert.IsFalse(cache.TryGetValue(items[i], out output));
+            }
+
+            // Next 100 items should still be there
+            for (int i = 100; i < 200; i++) {
+                Assert.IsTrue(cache.TryGetValue(items[i], out output));
+            }
+        }
+
+        [Test]
+        public void CheckLRU() {
+
+            int limit = 100 * 256;
+            var cache = new Cache<ByteArray>(limit, ba => ba.Length);
+            var items = new List<ByteArray>();
+
+            for (int i = 0; i < 100; i++) {
+                var key = ByteArray.Random(40);
+                var val = ByteArray.Random(256);
+                cache.Set(key, val);
+
+                items.Add(key);
+            }
+
+            ByteArray output;
+            // Touch the first 50 items again
+            for (int i = 0; i < 50; i++) {
+                Assert.IsTrue(cache.TryGetValue(items[i], out output));
+            }
+
+            // Add 10 more items
+            for (int i = 0; i < 10; i++) {
+                var key = ByteArray.Random(40);
+                var val = ByteArray.Random(256);
+                cache.Set(key, val);
+
+                items.Add(key);
+            }
+
+            // First 50 items should still be there
+            for (int i = 0; i < 50; i++) {
+                Assert.IsTrue(cache.TryGetValue(items[i], out output));
+            }
+
+            // Next 10 items should be evicted
+            for (int i = 50; i < 60; i++) {
+                Assert.IsFalse(cache.TryGetValue(items[i], out output));
+            }
+
+            // Next 50 items should still be there
+            for (int i = 60; i < 110; i++) {
+                Assert.IsTrue(cache.TryGetValue(items[i], out output));
+            }
+
+            Assert.GreaterOrEqual(limit, cache.CurrentSize);
+       }
+
+    }
+}
