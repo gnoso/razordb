221467250bb7f7c6fc57c9921c7e9c116cdc5970
Peter Waldschmidt <peterw@gnoso.com>
2013-01-03 18:41:25 -0500

    Add Buffer Pool. Prepare for processing pipeline.

---
+++ RazorDB/Pipeline.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace RazorDB {
+
+    // Manage a buffer pool to save allocation costs by reusing buffer blocks
+    // For simplicity we assume that all buffers are the same size
+    public class BufferPool {
+
+        public BufferPool(int bufferSize) {
+            size = bufferSize;
+            buffers = new LinkedList<byte[]>();
+        }
+        private int size;
+        private LinkedList<byte[]> buffers;
+        private int numAllocations = 0;
+
+        public int NumAllocations {
+            get { return numAllocations; }
+        }
+
+        public byte[] GetBuffer() {
+            lock (buffers) {
+                if (buffers.First == null) {
+                    numAllocations++;
+                    return new byte[size];
+                } else {
+                    var val = buffers.First.Value;
+                    buffers.RemoveFirst();
+                    return val;
+                }
+            }
+        }
+
+        public void ReturnBuffer(byte[] buffer) {
+            // memset(0) the buffer
+            Array.Clear(buffer, 0, buffer.Length);
+            // add buffer back to the buffer pool
+            lock (buffers) {
+                buffers.AddLast(buffer);
+            }
+        }
+    }
+
+    public interface ProcessingSegment {
+
+    }
+
+    public class Pipeline {
+
+
+    }
+}
---
+++ RazorDBTests/PipelineTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using RazorDB;
+using System.Threading;
+
+namespace RazorDBTests {
+    [TestFixture]
+    public class PipelineTests {
+
+        [Test]
+        public void BufferPool() {
+
+            BufferPool bp = new BufferPool(10000);
+            int ct = 1000;
+            for (int i = 0; i < ct; i++) {
+                byte[] b = bp.GetBuffer();
+                Assert.AreEqual(10000, b.Length);
+                bp.ReturnBuffer(b);
+            }
+            Console.WriteLine("{0} allocations for {1} uses of buffer pool.", bp.NumAllocations, ct);
+        }
+    }
+}
