cc60f1bb38a017a415deaa4c6d901e1c5b3afe72
Peter Waldschmidt <peterw@gnoso.com>
2013-01-03 15:14:47 -0500

    Fix for new journal tests in R02 branch.

--- RazorDBTests/JournalTests.cs
+++ RazorDBTests/JournalTests.cs
@@ -61,19 +61,19 @@ namespace RazorDBTests {
                 Directory.CreateDirectory(path);
             JournalWriter jw = new JournalWriter(path, 324, false);
 
-            List<KeyValuePair<Key, Value>> items = new List<KeyValuePair<Key, Value>>();
+            List<KeyValuePair<KeyEx, Value>> items = new List<KeyValuePair<KeyEx, Value>>();
             for (int i = 0; i < 10; i++) {
-                Key randKey = Key.Random(20);
+                KeyEx randKey = KeyEx.Random(20);
                 Value randValue = Value.Random(100);
                 jw.Add(randKey, randValue);
-                items.Add(new KeyValuePair<Key, Value>(randKey, randValue));
+                items.Add(new KeyValuePair<KeyEx, Value>(randKey, randValue));
             }
             jw.Close();
 
             // Reopen the file and add a partial record
             var fileName = Config.JournalFile(path, 324);
             var writer = new BinaryWriter(new FileStream(fileName, FileMode.Append, FileAccess.Write, FileShare.None, 1024, false));
-            Key key = Key.Random(20);
+            KeyEx key = KeyEx.Random(20);
             Value value = Value.Random(100);
             writer.Write7BitEncodedInt(key.Length);
             writer.Write(key.InternalBytes);
