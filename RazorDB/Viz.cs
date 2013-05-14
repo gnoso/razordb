77d740d161908cb936583561bac0b0c47c4af0d1
Taylor <taylorshuler@aol.com>
2013-05-14 17:51:40 -0400

    Updated in-text license statements

--- RazorDB/Viz.cs
+++ RazorDB/Viz.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*
+Copyright 2012, 2013 Gnoso Inc.
+
+This software is licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except for what is in compliance with the License.
+
+You may obtain a copy of this license at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either expressed or implied.
+
+See the License for the specific language governing permissions and limitations.
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
