4a354f5d51a6d9ca98fb3d342eb38880d5e44e1b
Taylor <taylorshuler@aol.com>
2013-05-27 12:27:13 -0400

    Moved projects for testing and debugging to a workspace

---
+++ RazorDB.Debug/RazorDBTests/RazorDBTests/ByteArrayTests.cs
@@ -0,0 +1,112 @@
+﻿/*
+Copyright 2012, 2013 Gnoso Inc.
+
+This software is licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except for what is in compliance with the License.
+
+You may obtain a copy of this license at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either expressed or implied.
+
+See the License for the specific language governing permissions and limitations.
+*/
+using NUnit.Framework;
+using RazorDB;
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace RazorDBTests
+{
+	[TestFixture]
+    public class ByteArrayTests
+	{
+		[Test]
+        public void Comparison ()
+		{
+			var a0 = new ByteArray (new byte[] { 0 });
+			var a2 = new ByteArray (new byte[] { 0, 1, 2, 4 });
+			var a2B = new ByteArray (new byte[] { 0, 1, 2, 4 });
+			var a3 = new ByteArray (new byte[] { 0, 1, 2, 5 });
+			var a4 = new ByteArray (new byte[] { 0, 1, 2, 5, 6 });
+
+			// Simple direct comparisons
+			Assert.IsTrue (a2.CompareTo(a3) < 0);
+			Assert.IsTrue (a3.CompareTo(a2) > 0);
+			Assert.IsTrue (a2.CompareTo(a2B) == 0);
+
+			// Length comparisons
+			Assert.IsTrue (a4.CompareTo(a0) > 0);
+			Assert.IsTrue (a3.CompareTo(a4) < 0);
+		}
+
+		[Test]
+        public void TestComparisonSpeed ()
+		{
+			var a = new ByteArray (new byte[] { 0 });
+			var b = new ByteArray (new byte[] { 1 });
+
+			Stopwatch timer = new Stopwatch ();
+			timer.Start ();
+
+			for (int i = 0; i < 1000000; i++) {
+				Assert.True (a.CompareTo(b) < 0);
+			}
+
+			timer.Stop ();
+			Console.WriteLine ("Elapsed Time (1 byte): {0} ms", timer.ElapsedMilliseconds);
+
+			a = new ByteArray (new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 });
+			b = new ByteArray (new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20 });
+
+			timer = new Stopwatch ();
+			timer.Start ();
+
+			for (int i = 0; i < 1000000; i++) {
+				Assert.True (a.CompareTo(b) < 0);
+			}
+
+			timer.Stop ();
+			Console.WriteLine ("Elapsed Time (20 byte): {0} ms", timer.ElapsedMilliseconds);
+		}
+	}
+
+	[TestFixture]
+    public class KeyTests
+	{
+		[Test]
+        public void TestKey ()
+		{
+			ByteArray keyBytes = ByteArray.Random (10);
+			byte[] allBytes = new byte[keyBytes.Length + 1 ];
+			Array.Copy (keyBytes.InternalBytes, allBytes, keyBytes.Length);
+
+			var keys = new List<Key> ();
+
+			for (int i = 99; i >= 0; i--) {
+				keys.Add (new Key(keyBytes.InternalBytes, (byte)i));
+			}
+
+			keys.Sort ();
+			int j = 0;
+
+			foreach (var k in keys) {
+				Assert.AreEqual (11, k.Length);
+
+				allBytes [allBytes.Length-1] = (byte)j;
+				Assert.AreEqual (allBytes, k.InternalBytes);
+
+				Assert.AreEqual (j, k.SequenceNum);
+				j++;
+			}
+
+			var keyA = new Key (keyBytes.InternalBytes, 23);
+			var keyB = Key.FromBytes (keyA.InternalBytes);
+			Assert.AreEqual (keyA, keyB);
+		}
+	}
+}
---
+++ RazorDB.Debug/RazorDBTests/RazorDBTests/CrashTests.cs
@@ -0,0 +1,111 @@
+﻿/*
+Copyright 2012, 2013 Gnoso Inc.
+
+This software is licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except for what is in compliance with the License.
+
+You may obtain a copy of this license at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either expressed or implied.
+
+See the License for the specific language governing permissions and limitations.
+*/
+using NUnit.Framework;
+using RazorDB;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+using System.Reflection;
+
+namespace RazorDBTests
+{
+	[TestFixture]
+    public class CrashTests
+	{
+		[Test]
+        public void CrashTestOnMerge ()
+		{
+			Console c;
+
+			string path = Path.GetFullPath ("TestData\\CrashTestOnMerge");
+			using (var db = new KeyValueStore(path)) {
+				db.Truncate ();
+			}
+
+			var doneSetting = new EventWaitHandle (false, EventResetMode.ManualReset, "CrashTestOnMerge");
+			doneSetting.Reset ();
+
+			string testPath = Path.Combine (Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase), "RazorTest.exe");
+			var process = Process.Start (testPath, "CrashTestOnMerge");
+
+			doneSetting.WaitOne (30000);
+			process.Kill ();
+			process.WaitForExit ();
+            
+			// Open the database created by the other program.
+			using (var db = new KeyValueStore(path)) {
+
+				db.Manifest.Logger = (msg) => c.WriteLine (msg);
+
+				c.WriteLine ("Begin enumeration.");
+				ByteArray lastKey = new ByteArray ();
+				int ct = 0;
+				foreach (var pair in db.Enumerate()) {
+					ByteArray k = new ByteArray (pair.Key);
+					Assert.True (lastKey.CompareTo(k) < 0);
+					lastKey = k;
+					ct++;
+				}
+				Assert.AreEqual (50000, ct);
+				c.WriteLine ("Found {0} items in the crashed database.", ct);
+			}
+
+		}
+
+		[Test]
+        public void CrashTestBeforeMerge ()
+		{
+			Console c;
+
+			string path = Path.GetFullPath ("TestData\\CrashTestBeforeMerge");
+			using (var db = new KeyValueStore(path)) {
+				db.Truncate ();
+			}
+
+			var doneSetting = new EventWaitHandle (false, EventResetMode.ManualReset, "CrashTestBeforeMerge");
+			doneSetting.Reset ();
+
+			string testPath = Path.Combine (Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase), "RazorTest.exe");
+			var process = Process.Start (testPath, "CrashTestBeforeMerge");
+
+			doneSetting.WaitOne (30000);
+			process.Kill ();
+			process.WaitForExit ();
+
+			// Open the database created by the other program.
+			using (var db = new KeyValueStore(path)) {
+
+				db.Manifest.Logger = (msg) => c.WriteLine (msg);
+
+				c.WriteLine ("Begin enumeration.");
+				ByteArray lastKey = new ByteArray ();
+				int ct = 0;
+
+				foreach (var pair in db.Enumerate()) {
+					ByteArray k = new ByteArray (pair.Key);
+					Assert.True (lastKey.CompareTo(k) < 0);
+					lastKey = k;
+					ct++;
+				}
+
+				Assert.AreEqual (10000, ct);
+				c.WriteLine ("Found {0} items in the crashed database.", ct);
+			}
+		}
+	}
+}
