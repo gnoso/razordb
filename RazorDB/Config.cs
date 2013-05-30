df94f1eb2256b45bc44a964ba04264e846c68e00
Taylor <taylorshuler@aol.com>
2013-05-30 11:17:05 -0400

    Extracted Helper class to its own file

--- RazorDB/Config.cs
+++ RazorDB/Config.cs
@@ -87,72 +87,10 @@ namespace RazorDB {
             var msg = string.Format(formatStr, values);
             LogMessage(msg);
         }
+
         public static void LogError(string formatStr, params object[] values) {
             var msg = string.Format(formatStr, values);
             LogMessage("ERROR " + msg, true);
         }
-
-
-    }
-
-    public static class Helper {
-
-        public static int Encode7BitInt(byte[] workingArray, int value) {
-            int size = 0;
-            uint num = (uint)value;
-            while (num >= 0x80) {
-                workingArray[size] = (byte)(num | 0x80);
-                size++;
-                num = num >> 7;
-            }
-            workingArray[size] = (byte)num;
-            size++;
-            return size;
-        }
-
-        public static int Decode7BitInt(byte[] workingArray, ref int offset) {
-            byte b;
-            int val = 0;
-            int bits = 0;
-            do {
-                b = workingArray[offset];
-                offset++;
-                val |= (b & 0x7f) << bits;
-                bits += 7;
-            }
-            while ((b & 0x80) != 0);
-            return val;
-        }
-        public static int Read7BitEncodedInt(this BinaryReader rdr) {
-            return (int) rdr.Read7BitEncodedUInt();
-        }
-
-        public static uint Read7BitEncodedUInt(this BinaryReader rdr) {
-            byte b;
-            int val = 0;
-            int bits = 0;
-            do {
-                b = rdr.ReadByte();
-                val |= (b & 0x7f) << bits;
-                bits += 7;
-            }
-            while ((b & 0x80) != 0);
-            return (uint)val;
-        }
-
-        public static void Write7BitEncodedInt(this BinaryWriter wtr, int value) {
-            if (value < 0)
-                throw new InvalidDataException("Negative numbers are not supported.");
-            wtr.Write7BitEncodedUInt( (uint) value);
-        }
-        public static void Write7BitEncodedUInt(this BinaryWriter wtr, uint value) {
-            uint num = value;
-            while (num >= 0x80) {
-                wtr.Write((byte)(num | 0x80));
-                num = num >> 7;
-            }
-            wtr.Write((byte)num);
-        }
-
-    }
+	}
 }
