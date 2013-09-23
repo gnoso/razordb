c33f9751243c201fa7528b3387c44f1c6e39e5ce
John Fleisher <jfleisher@gnoso.com>
2013-09-23 07:41:53 -0400

    RazorPerformanceCounter class to allow calls to Increment() even when perf counters failed to initialize.

--- RazorDB/PerformanceCounters.cs
+++ RazorDB/PerformanceCounters.cs
@@ -22,56 +22,66 @@ namespace RazorDB {
 
     public static class PerformanceCounters {
 
-        private static PerformanceCounter _SBTReadMetadataCached;
-        public static PerformanceCounter SBTReadMetadataCached { get { if (_SBTReadMetadataCached == null) Initialize(); return _SBTReadMetadataCached; } }
+        private static RazorPerformanceCounter _SBTReadMetadataCached;
+        public static RazorPerformanceCounter SBTReadMetadataCached { get { if (_SBTReadMetadataCached == null) Initialize(); return _SBTReadMetadataCached; } }
 
-        private static PerformanceCounter _SBTReadMetadata;
-        public static PerformanceCounter SBTReadMetadata { get { if (_SBTReadMetadata == null) Initialize(); return _SBTReadMetadata; } }
+        private static RazorPerformanceCounter _SBTReadMetadata;
+        public static RazorPerformanceCounter SBTReadMetadata { get { if (_SBTReadMetadata == null) Initialize(); return _SBTReadMetadata; } }
 
-        private static PerformanceCounter _SBTConstructed;
-        public static PerformanceCounter SBTConstructed { get { if (_SBTConstructed == null) Initialize(); return _SBTConstructed; } }
+        private static RazorPerformanceCounter _SBTConstructed;
+        public static RazorPerformanceCounter SBTConstructed { get { if (_SBTConstructed == null) Initialize(); return _SBTConstructed; } }
 
-        private static PerformanceCounter _SBTEnumerateFromKey;
-        public static PerformanceCounter SBTEnumerateFromKey { get { if (_SBTEnumerateFromKey == null) Initialize(); return _SBTEnumerateFromKey; } }
+        private static RazorPerformanceCounter _SBTEnumerateFromKey;
+        public static RazorPerformanceCounter SBTEnumerateFromKey { get { if (_SBTEnumerateFromKey == null) Initialize(); return _SBTEnumerateFromKey; } }
 
-        private static PerformanceCounter _SBTGetBlockTableIndex;
-        public static PerformanceCounter SBTGetBlockTableIndex { get { if (_SBTGetBlockTableIndex == null) Initialize(); return _SBTGetBlockTableIndex; } }
+        private static RazorPerformanceCounter _SBTGetBlockTableIndex;
+        public static RazorPerformanceCounter SBTGetBlockTableIndex { get { if (_SBTGetBlockTableIndex == null) Initialize(); return _SBTGetBlockTableIndex; } }
 
-        private static PerformanceCounter _SBTLookup;
-        public static PerformanceCounter SBTLookup { get { if (_SBTLookup == null) Initialize(); return _SBTLookup; } }
+        private static RazorPerformanceCounter _SBTLookup;
+        public static RazorPerformanceCounter SBTLookup { get { if (_SBTLookup == null) Initialize(); return _SBTLookup; } }
 
-        private static PerformanceCounter _SBTEnumerateMergedTablesPrecached;
-        public static PerformanceCounter SBTEnumerateMergedTablesPrecached { get { if (_SBTEnumerateMergedTablesPrecached == null) Initialize(); return _SBTEnumerateMergedTablesPrecached; } }
+        private static RazorPerformanceCounter _SBTEnumerateMergedTablesPrecached;
+        public static RazorPerformanceCounter SBTEnumerateMergedTablesPrecached { get { if (_SBTEnumerateMergedTablesPrecached == null) Initialize(); return _SBTEnumerateMergedTablesPrecached; } }
 
         private const string perfCategoryName = "RazorDb";
         private static void Initialize() {
-            // remove any previous definitions
-            if (PerformanceCounterCategory.Exists(perfCategoryName))
-                PerformanceCounterCategory.Delete(perfCategoryName);
-            
-            AddPerformanceCounterData("SBTConstructed", "Number of times SBT constructor is called", PerformanceCounterType.NumberOfItems64);
-            AddPerformanceCounterData("SBTReadMetadata", "Number of times ReadMetadata goes to disk", PerformanceCounterType.NumberOfItems64);
-            AddPerformanceCounterData("SBTReadMetadata Cached","Number of times ReadMetadata comeds from cache",PerformanceCounterType.NumberOfItems64);
-            AddPerformanceCounterData("SBTEnumerateFromKey", "Number of SBT created for EnumerateFromKey", PerformanceCounterType.NumberOfItems64);
-            AddPerformanceCounterData("SBTGetBlockTableIndex", "Number of SBT created for GetBlockTableIndex", PerformanceCounterType.NumberOfItems64);
-            AddPerformanceCounterData("SBTLookup", "Number of SBT created for Lookup", PerformanceCounterType.NumberOfItems64);
-            AddPerformanceCounterData("SBTEnumerateMergedTablesPrecached", "Number of SBT created for EnumerateMergedTablesPrecached", PerformanceCounterType.NumberOfItems64);
+            try {
+                // remove any previous definitions
+                if (PerformanceCounterCategory.Exists(perfCategoryName))
+                    PerformanceCounterCategory.Delete(perfCategoryName);
 
-            // Create the category and pass the collection to it.
-            System.Diagnostics.PerformanceCounterCategory.Create(
-                perfCategoryName, "Peformance counters for internal operations of RazorDb",
-                PerformanceCounterCategoryType.SingleInstance, _ccData);
+                AddPerformanceCounterData("SBTConstructed", "Number of times SBT constructor is called", PerformanceCounterType.NumberOfItems64);
+                AddPerformanceCounterData("SBTReadMetadata", "Number of times ReadMetadata goes to disk", PerformanceCounterType.NumberOfItems64);
+                AddPerformanceCounterData("SBTReadMetadata Cached", "Number of times ReadMetadata comeds from cache", PerformanceCounterType.NumberOfItems64);
+                AddPerformanceCounterData("SBTEnumerateFromKey", "Number of SBT created for EnumerateFromKey", PerformanceCounterType.NumberOfItems64);
+                AddPerformanceCounterData("SBTGetBlockTableIndex", "Number of SBT created for GetBlockTableIndex", PerformanceCounterType.NumberOfItems64);
+                AddPerformanceCounterData("SBTLookup", "Number of SBT created for Lookup", PerformanceCounterType.NumberOfItems64);
+                AddPerformanceCounterData("SBTEnumerateMergedTablesPrecached", "Number of SBT created for EnumerateMergedTablesPrecached", PerformanceCounterType.NumberOfItems64);
+
+                try {
+                    // Create the category and pass the collection to it.
+                    System.Diagnostics.PerformanceCounterCategory.Create(
+                        perfCategoryName, "Peformance counters for internal operations of RazorDb",
+                        PerformanceCounterCategoryType.SingleInstance, _ccData);
+                } catch {
+                    // not sure what to do with this exception.
+                    // have only seen an exception here when
+                }
+            } catch {
+                // exception at this level probably means lack of permissions
+            }
 
             // Create static counter refs
-            _SBTConstructed = new PerformanceCounter(perfCategoryName, "SBTConstructed", false);
-            _SBTEnumerateFromKey = new PerformanceCounter(perfCategoryName, "SBTEnumerateFromKey", false);
-            _SBTEnumerateMergedTablesPrecached = new PerformanceCounter(perfCategoryName, "SBTEnumerateMergedTablesPrecached", false);
-            _SBTGetBlockTableIndex = new PerformanceCounter(perfCategoryName, "SBTGetBlockTableIndex", false);
-            _SBTLookup = new PerformanceCounter(perfCategoryName, "SBTLookup", false);
-            _SBTReadMetadata = new PerformanceCounter(perfCategoryName, "SBTReadMetadata", false);
-            _SBTReadMetadataCached = new PerformanceCounter(perfCategoryName, "SBTReadMetadataCached",false);
+            _SBTConstructed = new RazorPerformanceCounter(perfCategoryName, "SBTConstructed", false);
+            _SBTEnumerateFromKey = new RazorPerformanceCounter(perfCategoryName, "SBTEnumerateFromKey", false);
+            _SBTEnumerateMergedTablesPrecached = new RazorPerformanceCounter(perfCategoryName, "SBTEnumerateMergedTablesPrecached", false);
+            _SBTGetBlockTableIndex = new RazorPerformanceCounter(perfCategoryName, "SBTGetBlockTableIndex", false);
+            _SBTLookup = new RazorPerformanceCounter(perfCategoryName, "SBTLookup", false);
+            _SBTReadMetadata = new RazorPerformanceCounter(perfCategoryName, "SBTReadMetadata", false);
+            _SBTReadMetadataCached = new RazorPerformanceCounter(perfCategoryName, "SBTReadMetadataCached", false);
 
         }
+
 
         private static CounterCreationDataCollection _ccData = new CounterCreationDataCollection();
         private static void AddPerformanceCounterData(string name, string help, PerformanceCounterType type) {
@@ -83,4 +93,27 @@ namespace RazorDB {
             _ccData.Add(ccd);
         }
     }
+
+    /// <summary>
+    /// RazorPerformanceCounter will allow calls to Increment
+    /// even if the true performance counter can't be created
+    /// in windows.
+    /// </summary>
+    public class RazorPerformanceCounter {
+        private PerformanceCounter _perfCtr;
+
+        public RazorPerformanceCounter(string category, string name, bool readOnly) {
+            try {
+                _perfCtr = new PerformanceCounter(category, name, readOnly);
+            } catch {
+                _perfCtr = null;
+            }
+        }
+
+        public void Increment() {
+            if (_perfCtr != null)
+                _perfCtr.Increment();
+        }
+    }
+
 }
