8536d091fa77fba37b90b478c3ee8c524102e0fd
JAF <jfleisher@gnoso.com>
2016-09-02 10:41:02 -0400

    SPIKE: read ahead with CircularBuffer

--- RazorDB/KeyValueStore.cs
+++ RazorDB/KeyValueStore.cs
@@ -349,54 +349,35 @@ namespace RazorDB {
         public IEnumerable<KeyValuePair<byte[], byte[]>> Find(string indexName, byte[] lookupValue) {
 
             KeyValueStore indexStore = GetSecondaryIndex(indexName);
-            var cacheLock = new object();
-            var cache = new Queue<KeyValuePair<byte[], byte[]>>();
-
-            ManualResetEvent readComplete = new ManualResetEvent(false);
-
-            var wkItem = indexingThreads.QueueWorkItem(() => {
-                // Loop over the values
-                foreach (var pair in indexStore.EnumerateFromKey(lookupValue)) {
-                    var key = pair.Key;
-                    var value = pair.Value;
-                    // construct our index key pattern (lookupvalue | key)
-                    if (ByteArray.CompareMemCmp(key, 0, lookupValue, 0, lookupValue.Length) == 0) {
-                        int offset = 0;
-                        byte[] objectKey = null;
-                        if (indexStore.RazorFormatVersion < 2) {
-                            if (ByteArray.CompareMemCmp(key, key.Length - value.Length, value, 0, value.Length) == 0)
-                                objectKey = pair.Value;
-                        } else {
-                            int indexKeyLen = Helper.Decode7BitInt(pair.Value, ref offset);
-                            if (lookupValue.Length == indexKeyLen) {
-                                // Lookup the value of the actual object using the key that was found
-                                // get the object key from the index value tail
-                                objectKey = ItemKeyFromIndex(pair, indexKeyLen);
-                            }
-                        }
-                        if (objectKey != null) {
-                            var primaryValue = this.Get(objectKey);
-                            if (primaryValue != null)
-                                lock(cacheLock)
-                                    cache.Enqueue(new KeyValuePair<byte[], byte[]>(objectKey, primaryValue));
-                        }
+            // Loop over the values
+            foreach (var pair in indexStore.EnumerateFromKey(lookupValue)) {
+                var key = pair.Key;
+                var value = pair.Value;
+                // construct our index key pattern (lookupvalue | key)
+                if (ByteArray.CompareMemCmp(key, 0, lookupValue, 0, lookupValue.Length) == 0) {
+                    int offset = 0;
+                    byte[] objectKey = null;
+                    if (indexStore.RazorFormatVersion < 2) {
+                        if (ByteArray.CompareMemCmp(key, key.Length - value.Length, value, 0, value.Length) == 0)
+                            objectKey = pair.Value;
                     } else {
-                        // if the above condition was not met then we must have enumerated past the end of the indexed value
-                        break;
+                        int indexKeyLen = Helper.Decode7BitInt(pair.Value, ref offset);
+                        if (lookupValue.Length == indexKeyLen) {
+                            // Lookup the value of the actual object using the key that was found
+                            // get the object key from the index value tail
+                            objectKey = ItemKeyFromIndex(pair, indexKeyLen);
+                        }
                     }
-                }
-            });
-
-            while (!readComplete.WaitOne(0)) {
-                while (cache.Any()) {
-                    lock (cacheLock)
-                        yield return (KeyValuePair<byte[], byte[]>)cache.Dequeue();
+                    if (objectKey != null) {
+                        var primaryValue = this.Get(objectKey);
+                        if (primaryValue != null)
+                            yield return new KeyValuePair<byte[], byte[]>(objectKey, primaryValue);
+                    }
+                } else {
+                    // if the above condition was not met then we must have enumerated past the end of the indexed value
+                    yield break;
                 }
             }
-
-            if (wkItem != null)
-                wkItem.Cancel(true);
-
             yield break;
         }
 
@@ -405,11 +386,9 @@ namespace RazorDB {
         public IEnumerable<KeyValuePair<byte[], byte[]>> FindStartsWith(string indexName, byte[] lookupValue) {
 
             KeyValueStore indexStore = GetSecondaryIndex(indexName);
-            var cacheLock = new object();
-            var cache = new Queue<KeyValuePair<byte[], byte[]>>();
 
-            ManualResetEvent readComplete = new ManualResetEvent(false);
-
+            var cache = new CircularBuffer<KeyValuePair<byte[], byte[]>>(1000);
+            bool done = false;
             var wkItem = indexingThreads.QueueWorkItem(() => {
                 // Loop over the values
                 foreach (var pair in indexStore.EnumerateFromKey(lookupValue)) {
@@ -432,22 +411,22 @@ namespace RazorDB {
                         if (objectKey != null) {
                             var primaryValue = Get(objectKey);
                             if (primaryValue != null) {
-                                lock (cacheLock)
-                                    cache.Enqueue(new KeyValuePair<byte[], byte[]>(objectKey, primaryValue));
+                                cache.Enqueue(new KeyValuePair<byte[], byte[]>(objectKey, primaryValue));
                             }
                         }
                     } else {
                         break;
                     }
                 }
-                readComplete.Set();
+                done = true;
             });
 
-            while (!readComplete.WaitOne(0)) {
-                while (cache.Any()) {
-                    lock (cacheLock)
-                        yield return (KeyValuePair<byte[], byte[]>)cache.Dequeue();
-                }
+
+            var buffer = new KeyValuePair<byte[], byte[]>[1000];
+            uint buffSz = 0;
+            while ((buffSz=cache.Dequeue(buffer)) > 0 || !done) {
+                for(int i=0;i<buffSz;i++)
+                    yield return buffer[i];
             }
 
             if (wkItem != null)
@@ -465,48 +444,27 @@ namespace RazorDB {
         public IEnumerable<KeyValuePair<byte[], byte[]>> FindKeysByIndexStartsWith(string indexName, byte[] lookupValue) {
 
             KeyValueStore indexStore = GetSecondaryIndex(indexName);
-            var cacheLock = new object();
-            var cache = new Queue<KeyValuePair<byte[], byte[]>>();
-
-            ManualResetEvent readComplete = new ManualResetEvent(false);
-
-            var wkItem = indexingThreads.QueueWorkItem(() => {
-                // Loop over the values
-                foreach (var pair in indexStore.EnumerateFromKey(lookupValue)) {
-                    // construct our index key pattern (lookupvalue | key)
-                    if (ByteArray.CompareMemCmp(pair.Key, 0, lookupValue, 0, lookupValue.Length) == 0) {
-                        int offset = 0;
-                        if (Manifest.RazorFormatVersion < 2) {
-                            if (ByteArray.CompareMemCmp(pair.Key, pair.Key.Length - pair.Value.Length, pair.Value, 0, pair.Value.Length) == 0)
-                                lock (cacheLock)
-                                    cache.Enqueue(new KeyValuePair<byte[], byte[]>(pair.Key, pair.Value));
-                        } else {
-                            int indexKeyLen = Helper.Decode7BitInt(pair.Value, ref offset);
-                            if (lookupValue.Length <= indexKeyLen) {
-                                var objectKey = ItemKeyFromIndex(pair, indexKeyLen);
-                                Helper.BlockCopy(pair.Key, indexKeyLen, objectKey, 0, pair.Key.Length - indexKeyLen);
-                                lock (cacheLock)
-                                    cache.Enqueue(new KeyValuePair<byte[], byte[]>(pair.Key, objectKey));
-                            }
-                        }
+            // Loop over the values
+            foreach (var pair in indexStore.EnumerateFromKey(lookupValue)) {
+                // construct our index key pattern (lookupvalue | key)
+                if (ByteArray.CompareMemCmp(pair.Key, 0, lookupValue, 0, lookupValue.Length) == 0) {
+                    int offset = 0;
+                    if (Manifest.RazorFormatVersion < 2) {
+                        if (ByteArray.CompareMemCmp(pair.Key, pair.Key.Length - pair.Value.Length, pair.Value, 0, pair.Value.Length) == 0)
+                            yield return new KeyValuePair<byte[], byte[]>(pair.Key, pair.Value);
                     } else {
-                        // if the above condition was not met then we must have enumerated past the end of the indexed value
-                        break;
+                        int indexKeyLen = Helper.Decode7BitInt(pair.Value, ref offset);
+                        if (lookupValue.Length <= indexKeyLen) {
+                            var objectKey = ItemKeyFromIndex(pair, indexKeyLen);
+                            Helper.BlockCopy(pair.Key, indexKeyLen, objectKey, 0, pair.Key.Length - indexKeyLen);
+                            yield return new KeyValuePair<byte[], byte[]>(pair.Key, objectKey);
+                        }
                     }
-                }
-            });
-
-            while (!readComplete.WaitOne(0)) {
-                while (cache.Any()) {
-                    lock (cacheLock)
-                        yield return (KeyValuePair<byte[], byte[]>)cache.Dequeue();
+                } else {
+                    // if the above condition was not met then we must have enumerated past the end of the indexed value
+                    yield break;
                 }
             }
-
-            if (wkItem != null)
-                wkItem.Cancel(true);
-
-            yield break;
         }
 
         public void Delete(byte[] key) {
---
+++ RazorDB/ThreadSafeCircularQueue.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Threading;
+
+namespace RazorDB {
+    /// <summary>
+    /// Circular Buffer implementation that acts as a sliding window. New items are overwritting oldest
+    /// values. Readers can get access to the buffer by copying it to their own Thread Local Storage.
+    /// This implementation has been tested for thread safety and support multiple concurrent readers/writters.
+    /// Please talk to Bogdan before modifying this class.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class CircularBuffer<T> where T : struct
+    {
+        private int _lock;
+        private readonly uint _capacity;
+
+        private volatile uint _head;
+        private volatile uint _tail;
+        private readonly T[] _buffer;
+        private bool _isEmpty;
+
+        private CircularBuffer()
+        {
+        }
+
+        public CircularBuffer(uint capacity)
+        {
+            _capacity = capacity;
+            _buffer = new T[_capacity];
+            _head = _tail = 0;
+            _lock = 0;
+            _isEmpty = true;
+        }
+
+        /// <summary>
+        /// Returns the number of items present in the ring buffer.
+        /// </summary>
+        /// <returns></returns>
+        public uint GetLength()
+        {
+            if (_isEmpty)
+                return 0;
+
+            if (_tail < _head)
+                return _capacity - _head + _tail + 1;
+
+            return _tail - _head + 1;
+        }
+
+        public uint GetHead()
+        {
+            return _head;
+        }
+
+        public uint GetTail()
+        {
+            return _tail;
+        }
+
+        /// <summary>
+        /// Returns the maximum number of elements that the ring buffer can hold at
+        /// any point in time.
+        /// </summary>
+        /// <returns></returns>
+        public uint GetCapacity()
+        {
+            return _capacity;
+        }
+
+        /// <summary>
+        /// Enqueues one item at the "tail" of the ring buffer. If the buffer is full
+        /// the "head" element is overwritten.
+        /// </summary>
+        /// <param name="item">The item to enqueue.</param>
+        public void Enqueue(T item)
+        {
+            while (Interlocked.CompareExchange(ref _lock, 1, 0) != 0) ;
+            if (GetLength() == _capacity)
+            {
+                _head++;
+                if (_head == _capacity)
+                    _head = 0;
+            }
+            
+            if (!_isEmpty)
+                _tail++;
+            if (_tail == _capacity)
+                _tail = 0;
+            _buffer[_tail] = item;
+
+            _isEmpty = false;
+
+            if (Interlocked.CompareExchange(ref _lock, 0, 1) != 1)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        /// <summary>
+        /// Clears the circular buffer discarding all items
+        /// </summary>
+        public void Clear()
+        {
+            while (Interlocked.CompareExchange(ref _lock, 1, 0) != 0) ;
+            _head = _tail = 0;
+            _isEmpty = true;
+            if (Interlocked.CompareExchange(ref _lock, 0, 1) != 1)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        /// <summary>
+        /// Enqueues an array of items at the "tail" of the ring buffer. If the buffer is full
+        /// the elements at "head" will be overwritten.
+        /// </summary>
+        /// <param name="items">The array of items to be enqueued.</param>
+        public void Enqueue(T[] items)
+        {
+            if( items.Length > _capacity )
+                throw new InvalidOperationException(
+                    "You are trying to add too many items. The buffer's capacity will be exceeded.");
+
+            if (items.Length == 0)
+                return;
+
+            while (Interlocked.CompareExchange(ref _lock, 1, 0) != 0) ;
+
+            int howManyToAdd = items.Length;
+            uint availableSpace = _capacity - GetLength();
+
+            // if _head will be overwritten, go ahead and advance it
+            if( howManyToAdd - availableSpace > 0)
+            {
+                _head += ((uint)(howManyToAdd - availableSpace));
+                _head = _head % _capacity; // todo: replace slow operation?
+            }
+
+            if (!_isEmpty)
+                _tail++;
+
+            _isEmpty = false;
+
+            // make _tail point to where copying should start
+            if (_tail >= _capacity)
+                _tail = 0;
+
+            // if _tail is close to the end of the array
+            int howManyTillEnd = (_capacity - _tail) < howManyToAdd ? (int)(_capacity - _tail) : howManyToAdd;
+            Array.Copy(items,0,_buffer,_tail, howManyTillEnd);
+            _tail += (uint)howManyTillEnd-1;
+
+            if(_tail > _capacity)
+                throw new ArithmeticException("There is a bug in the ring buffer!");
+
+            if (howManyTillEnd < howManyToAdd)
+            {   
+                // prepare for another copy
+                _tail++;
+            }
+
+            if (_tail == _capacity)
+                _tail = 0;
+
+            if( howManyTillEnd < howManyToAdd )
+            {
+                Array.Copy(items,howManyTillEnd,_buffer,_tail, howManyToAdd - howManyTillEnd);
+                // _tail is protected from wrap around because we don't allow enqueuing of more items
+                // than the _capacity of the buffer
+                _tail += (uint) (howManyToAdd - howManyTillEnd -1);
+            }
+            
+            if (Interlocked.CompareExchange(ref _lock, 0, 1) != 1)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        /// <summary>
+        /// Makes a copy of the ring buffer.
+        /// </summary>
+        /// <param name="destination">The destination array where the ring buffer's items will be copied.</param>
+        /// <param name="head">The "head" of the ring buffer.</param>
+        /// <param name="tail">The "tail" of the ring buffer.</param>
+        public void Copy(Array destination, out uint head, out uint tail)
+        {
+            if (destination.Length < _capacity)
+            {
+                head = 0;
+                tail = 0;
+                return;
+            }
+            while (Interlocked.CompareExchange(ref _lock, 1, 0) != 0) ;
+            Array.Copy(_buffer, destination, _capacity);
+            head = _head;
+            tail = _tail;
+
+            if (Interlocked.CompareExchange(ref _lock, 0, 1) != 1)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        /// <summary>
+        /// Discards "howMany" items from the head of the circular buffer and advances the head
+        /// </summary>
+        /// <param name="howMany">How many items to discard.</param>
+        /// <returns></returns>
+        public uint Discard(int howMany)
+        {
+            if (_isEmpty)
+                return 0;
+
+            while (Interlocked.CompareExchange(ref _lock, 1, 0) != 0) ;
+            uint available = GetLength();
+            uint howManyToDiscard;
+
+            if( howMany >= available )
+            {
+                howManyToDiscard = available;
+                _isEmpty = true;
+            }
+            else
+            {
+                howManyToDiscard = (uint)howMany;
+            }
+
+            if (_tail < _head)
+            {
+                uint howManyTillEnd = (_capacity - _head) > howManyToDiscard ? howManyToDiscard : (_capacity - _head);
+                _head += howManyTillEnd;
+                if (_head > _capacity)
+                    throw new ArithmeticException("There is a bug in the ring buffer!");
+                if (_head == _capacity)
+                    _head = 0;
+                if (howManyToDiscard> howManyTillEnd)
+                {
+                    _head += howManyToDiscard- howManyTillEnd;
+                }
+            }
+            else
+            {
+                if (_tail - _head + 1 < howManyToDiscard)
+                    throw new ArithmeticException("There is a bug in the ring buffer!");
+
+                _head += howManyToDiscard;
+            }
+
+
+            if (_isEmpty)
+                _head = _tail = 0;
+
+            if (Interlocked.CompareExchange(ref _lock, 0, 1) != 1)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return howManyToDiscard;
+        }
+
+        /// <summary>
+        /// The method will dequeue as many items from the circular buffer as the Length of
+        /// the destination array. If not enough items are available, the destination array will be filled
+        /// with the available items.
+        /// </summary>
+        /// <param name="destination">The destination array in which items will be dequeued.</param>
+        /// <returns>The number of items that were actually returned.</returns>
+        public uint Dequeue(Array destination)
+        {
+            if (_isEmpty)
+                return 0;
+
+            while (Interlocked.CompareExchange(ref _lock, 1, 0) != 0) ;
+            uint available = GetLength();
+            uint howManyToDequeue;
+
+            // determine how many items we can actually dequeue
+            if( destination.Length >= available )
+            {
+                howManyToDequeue = available;
+                _isEmpty = true;
+            }
+            else
+            {
+                howManyToDequeue = (uint)destination.Length;
+            }
+            
+            if( _tail < _head )
+            {
+                uint howManyTillEnd = _capacity - _head > howManyToDequeue ? howManyToDequeue : _capacity - _head;
+                Array.Copy(_buffer, _head, destination, 0, howManyTillEnd);
+                _head += howManyTillEnd;
+                if( _head > _capacity )
+                    throw new ArithmeticException("There is a bug in the ring buffer!");
+                if (_head == _capacity)
+                    _head = 0;
+                if( howManyToDequeue > howManyTillEnd )
+                {
+                    Array.Copy(_buffer, _head, destination, howManyTillEnd, howManyToDequeue - howManyTillEnd);
+                    _head += howManyToDequeue - howManyTillEnd;
+                }
+            }
+            else
+            {
+                if( _tail - _head + 1 < howManyToDequeue )
+                    throw new ArithmeticException("There is a bug in the ring buffer!");
+
+                Array.Copy(_buffer,_head, destination, 0, howManyToDequeue);
+                _head += howManyToDequeue;
+            }
+
+            if (_isEmpty)
+                _head = _tail = 0;
+
+            if (Interlocked.CompareExchange(ref _lock, 0, 1) != 1)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return howManyToDequeue;
+        }
+
+        internal bool IsFull() {
+            return _capacity == GetLength();
+        }
+    }
+
+}
+
